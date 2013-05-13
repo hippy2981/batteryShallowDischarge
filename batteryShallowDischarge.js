4a529c4da8a3ea9cb72480c99bc308e50de51de8
hippy <hippy2981@gmail.com>
2013-05-12 19:16:10 -0700

    Fixing excessive notifications.

--- batteryShallowDischarge.js
+++ batteryShallowDischarge.js
@@ -1,23 +1,54 @@
 console.log('Starting rule: batteryShallowDischarge');
 
+// battery level limts
+var lowerLimit = 40;
+var upperLimit = 80;
+var criticalLimit = 5;
+// notify interval in seconds
+var notifyInterval = 30;
+// Key(s) used for local storage.
+var lastNotifiedKey = 'batteryShallowDischarge.lastNotified';
+
 device.battery.on('updated', function (status) {
-    // note: status.isCharging does not seem to work correctly.
-    var isOffLimitsBetter = 
-        ((!status.isCharging && status.percentage == 40) ||
-			(status.isCharging && status.percentage == 80));
-    var isOffLimits = ((status.percentage == 40) || (status.percentage == 80));
-    var isCharged = status.percentage == 100;
-    var isDischarged = status.percentage <= 5;
-	if (isOffLimits || isOffLimitsBetter || isCharged || isDischarged) {
-		device.notifications
-			.createNotification("battery charge: " + status.percentage + "%")
-			.show();
-	}
+    // notify to plug/unplug
+    var isOffLimits = (
+        ((!status.isCharging && status.percentage <= lowerLimit)
+            || (status.isCharging && status.percentage >= upperLimit))
+        // notify every 5% only
+        && status.percentage % 5 === 0
+        // hack: avoid multiple notifications due to multiple 'updated' triggers fired
+        && !isNotifiedRecently()
+    );
+    // notify to unplug
+    var isCharged = status.isCharging && status.percentage === 100;
+    // notify to plug
+    var isDischarged = !status.isCharging && status.percentage <= criticalLimit;
+    if (isOffLimits || isCharged || isDischarged) {
+        device.notifications
+            .createNotification("battery charge: " + status.percentage + "%")
+            .show();
+        saveLastNotified();
+    }
 });
 
+// Returns true if notified in last few seconds.
+function isNotifiedRecently() {
+    var isNotified = false;
+    var lastNotified = device.localStorage.getItem(lastNotifiedKey);
+    if (lastNotified) {
+        var seconds = (new Date().getTime() - lastNotified) / 1000;
+        isNotified = seconds <= notifyInterval;
+    }
+    return isNotified;
+}
+
+// Saves current datetime to local storage.
+function saveLastNotified() {
+    device.localStorage.setItem(lastNotifiedKey, new Date().getTime());
+}
+
 // tests
-//device.battery.emit('updated', { percentage: 80 });
-//device.battery.emit('updated', { percentage: 40 });
+//device.battery.emit('updated', { percentage: lowerLimit });
+//device.battery.emit('updated', { percentage: upperLimit });
 
 console.log('Completed rule: batteryShallowDischarge');
-
