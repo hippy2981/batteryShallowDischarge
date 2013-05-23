70487285178a73759482182c3865ace709146779
hippy <hippy2981@gmail.com>
2013-05-23 11:49:04 -0700

    Notify as per monthly cycle.

--- README.md
+++ README.md
@@ -1,6 +1,58 @@
 batteryShallowDischarge
 =======================
 
-on{X} javascript script to help with shallow battery discharges by notifying when device battery level reaches 40% or 80%.
+on{X} javascript rule to help with shallow battery discharges.
 
-Note: Need on{X} app on device. 
+### What is a shallow battery discharge?
+Keep the battery level between 40% and 80%. Do a full discharge once a month. Read more about what shallow battery discharge is and why to do it at this lifehacker article [How Often Should I Charge My Gadget's Battery to Prolong Its Lifespan?](http://lifehacker.com/5875162/how-often-should-i-charge-my-gadgets-battery-to-prolong-its-lifespan)
+
+### What does the script do?
+
+This script prompts you to connect or disconnect charger by doing below things.
+
+1. Notifies when battery level reaches the shallow discharge range limits 40% or 80%. 
+2. Notifies when battery level is fully charged or discharged so that you can connect or disconnect your charger. 
+3. Notifies you to fully discharge if you're not doing so after a month of shallow discharges. The script keeps track of the monthly cycle so you don't have to. 
+4. Notifies you to unplug if you connect charger without the battery reaching 40% (similarly for 80% too). The script prompts you to correctly do shallow discharges. 
+
+##### Battery Shallow Discharge workflow:
+
+![battery shallow discharge workflow](battery-shallow-discharge-workflow.png "battery shallow discharge workflow")
+
+### How do the notifications look like?
+
+1. Notification when charging and battery level reaches 80%.
+
+        Unplug.
+        Battery: 80%
+
+2. Notification when discharging and battery level reaches 40%.
+
+        Plug in.
+        Battery: 40%
+
+3. Notification when device is plugged without doing a full discharge and current battery is at 70%.
+
+        Discharge fully.
+        Battery: 70%
+
+### How do I get this on my phone?
+
+You need the on{X} app installed. You can read about it on the [on{X} site](https://www.onx.ms/#!landingPage). As of today, on{X} is available on Android and NOT on iOS and Windows. 
+
+### This stuff is not working. What do I do?
+You can set the `debug` variable and look at the script's log on on{X} website. Remember to reset it as logging eats battery. 
+
+```JavaScript
+// set true for debug logs
+var debug = true;
+```
+
+### I want to change the 40% and 80% limits. What do I do?
+Change them in the script. Set them in increments of 5. 
+
+```JavaScript
+// battery level limts
+var lowerLimit = 40; //should be 5x
+var upperLimit = 80; //should be 5x
+```
--- batteryShallowDischarge.js
+++ batteryShallowDischarge.js
@@ -1,54 +1,279 @@
-console.log('Starting rule: batteryShallowDischarge');
+//{/////////////// debug /////////////////
+
+// set true for debug logs
+var debug = false;
+function consolelog(data) {
+    if (debug) {
+        console.log(data);
+    }
+}
+
+//}/////////////// debug /////////////////
+
+consolelog('####Starting rule: batteryShallowDischarge####');
+
+//{/////////////// settings /////////////////
 
 // battery level limts
-var lowerLimit = 40;
-var upperLimit = 80;
-var criticalLimit = 5;
+var lowerLimit = 40; //should be 5x
+var upperLimit = 80; //should be 5x
+var dischargedLimit = 5; //should be 5x
+var chargedLimit = 100; //should be 5x
 // notify interval in seconds
-var notifyInterval = 30;
-// Key(s) used for local storage.
-var lastNotifiedKey = 'batteryShallowDischarge.lastNotified';
+var notifyInterval = 10;
+// last notification time
+var lastNotified;
+// key(s) used for local storage
+var stateKey = 'bsd.cycle';
 
+//}/////////////// settings /////////////////
+
+//{/////////////// init /////////////////
+
+init();
+// Init.
+function init(){
+    // set lastNotified as now
+    resetLastNotified();
+    // reset cycle
+    resetCycle('no force');
+}
+
+//}/////////////// init /////////////////
+
+//{/////////////// events /////////////////
+
+// updated event
 device.battery.on('updated', function (status) {
-    // notify to plug/unplug
-    var isOffLimits = (
-        ((!status.isCharging && status.percentage <= lowerLimit)
-            || (status.isCharging && status.percentage >= upperLimit))
-        // notify every 5% only
-        && status.percentage % 5 === 0
-        // hack: avoid multiple notifications due to multiple 'updated' triggers fired
-        && !isNotifiedRecently()
-    );
-    // notify to unplug
-    var isCharged = status.isCharging && status.percentage === 100;
-    // notify to plug
-    var isDischarged = !status.isCharging && status.percentage <= criticalLimit;
-    if (isOffLimits || isCharged || isDischarged) {
-        device.notifications
-            .createNotification("battery charge: " + status.percentage + "%")
-            .show();
-        saveLastNotified();
+    if (status.testid) consolelog('test#' + status.testid);
+    consolelog('status: ' + 'isCharging=' + status.isCharging + 
+        ', status.percentage=' + status.percentage);
+    // notify every 5% only and avoid hitting local storage every %
+    if (status.percentage % 5 === 0) {
+        var state = getState();
+        checkToNotify(status, 'updated', state);
+        updateState(status, state);
     }
 });
 
-// Returns true if notified in last few seconds.
-function isNotifiedRecently() {
-    var isNotified = false;
-    var lastNotified = device.localStorage.getItem(lastNotifiedKey);
-    if (lastNotified) {
-        var seconds = (new Date().getTime() - lastNotified) / 1000;
-        isNotified = seconds <= notifyInterval;
+// *Charging events
+device.battery.on('startedCharging', function (status) {
+    // notify to unplug
+    checkToNotify(status, 'startedCharging');
+});
+device.battery.on('stoppedCharging', function (status) {
+    // notify to plug
+    checkToNotify(status, 'stoppedCharging');
+});
+
+//}/////////////// events /////////////////
+
+//{/////////////// functions /////////////////
+
+// Notify user to either fully charge/discharge if not doing so.
+function checkToNotify(status, event, state) {
+    state = state || getState();
+    var isChargingEvent = event.indexOf('Charging') >= 0;
+    var text = null;
+    var isInCycle = isWithinCycle(state);
+    var isCharged = 
+        status.isCharging && status.percentage === chargedLimit;
+    var isDischarged = 
+        !status.isCharging && status.percentage === dischargedLimit;
+    if (isCharged) {
+        text = 'Unplug.';
+    } else if (isDischarged) {
+        text = 'Plug in.';
+    } else if (!isInCycle && isChargingEvent) {
+        // *Charging event
+        if (status.isCharging) {
+            text = 'Discharge fully.';
+        }
+    } else if (isInCycle) {
+        // updated, *Charging events
+        if (!status.isCharging &&
+            (state.shallow === 'lower' || status.percentage <= lowerLimit)) {
+            text = 'Plug in.';
+        } else if (status.isCharging &&
+            (state.shallow === 'upper' || status.percentage >= upperLimit)) {
+            text = 'Unplug.';
+        }
     }
-    return isNotified;
+    if (text && !isNotifiedRecently()) {
+        notify(text, status);
+    }
 }
 
-// Saves current datetime to local storage.
-function saveLastNotified() {
-    device.localStorage.setItem(lastNotifiedKey, new Date().getTime());
+// Update the state to local storage.
+function updateState(status, state) {
+    consolelog('state (before): ' + stateLog());
+    if (!isWithinCycle(state)) {
+        // change state only if not within cycle 
+        // (ex: accidental full charge/discharge within cycle)
+        var isDischarged = 
+            !status.isCharging && status.percentage === dischargedLimit;
+        if (isDischarged) {
+            resetCycle();
+            consolelog('resetCycle()');
+        }
+    }
+    if (status.percentage === upperLimit) {
+        state.shallow = 'upper';
+        setState(state);
+        consolelog('shallow = upper');
+    } else if (status.percentage === lowerLimit) {
+        state.shallow = 'lower';
+        setState(state);
+        consolelog('shallow = lower');
+    }
+    consolelog('state (after): ' + stateLog());
 }
 
-// tests
-//device.battery.emit('updated', { percentage: lowerLimit });
-//device.battery.emit('updated', { percentage: upperLimit });
+// Notify.
+function notify(text, status) {
+    var content = 'Battery: ' + status.percentage + '%';
+    if (status.testid) content += (' test#' + status.testid);
+    var notification = device.notifications.createNotification(text);
+    notification.content = content;
+    notification.on('click', function() { });
+    notification.show();
+    consolelog('notify: ' + text + " | " + content);
+    resetLastNotified();
+}
 
-console.log('Completed rule: batteryShallowDischarge');
+// Returns true if in cycle.
+function isWithinCycle(state) {
+    var month = new Date().getMonth();
+    var result = month === state.cycleMonth;
+    consolelog('isWithinCycle: ' + result + ', month: ' + month);
+    return result;
+}
+
+// Resets cycle in local storage.
+function resetCycle(force, date) {
+    force = force === undefined;
+    date = date || new Date();
+    if (force || !getState()) {
+        var state = { cycleMonth: date.getMonth(), shallow: 'lower' };
+        setState(state);
+        consolelog('resetCycle. state: ' + stateLog());
+    } else {
+        consolelog('existing state: ' + stateLog());
+    }
+}
+
+// Get state from local storage.
+function getState() {
+    return JSON.parse(device.localStorage.getItem(stateKey));
+}
+
+// Set state to local storage.
+function setState(state) {
+    return device.localStorage.setItem(stateKey, JSON.stringify(state));
+}
+
+// Returns true if recently notified.
+function isNotifiedRecently() {
+    var seconds = (new Date().getTime() - lastNotified.getTime()) / 1000;
+    return seconds <= notifyInterval;
+}
+
+// Resets last notified to now.
+function resetLastNotified() {
+    lastNotified = new Date();
+}
+
+// Get state for logging.
+function stateLog(state) {
+    state = state || getState();
+    return JSON.stringify(state);
+}
+
+//}/////////////// functions /////////////////
+
+consolelog('####Completed rule: batteryShallowDischarge####');
+
+//{/////////////// tests /////////////////
+/*
+
+consolelog('tests start');
+
+// get the old state to revert to after tests are run
+var oldstate = getState();
+
+notifyInterval = -1; // disable notifyInterval
+
+// in cycle: notify only at 40 d/c and 80 c
+setState({ cycleMonth: new Date().getMonth() });
+device.battery.emit('updated', { testid: 1, percentage: lowerLimit, isCharging: true });
+device.battery.emit('updated', { testid: 2, percentage: lowerLimit, isCharging: false });//notify to plug
+device.battery.emit('updated', { testid: 3, percentage: upperLimit, isCharging: true });//notify to unplug
+device.battery.emit('updated', { testid: 4, percentage: upperLimit, isCharging: false });
+
+// NOT in cycle: no notify at 40 d/c and 80 c
+setState({ cycleMonth: new Date().getMonth()-1 });
+device.battery.emit('updated', { testid: 5, percentage: lowerLimit, isCharging: false });
+device.battery.emit('updated', { testid: 6, percentage: upperLimit, isCharging: true });
+
+// c limit: notify always in cycle or not but not when not c
+setState({ cycleMonth: new Date().getMonth() });
+device.battery.emit('updated', { testid: 7, percentage: 100, isCharging: true });//notify to unplug
+device.battery.emit('updated', { testid: 8, percentage: 100, isCharging: false });
+setState({ cycleMonth: new Date().getMonth()-1 });
+device.battery.emit('updated', { testid: 9, percentage: 100, isCharging: true });//notify to unplug
+device.battery.emit('updated', { testid:10, percentage: 100, isCharging: false });
+
+// NOT in cycle: notify at d/c limit and reset cycle only when d/c
+setState({ cycleMonth: new Date().getMonth()-1 });
+device.battery.emit('updated', { testid:11, percentage: dischargedLimit, isCharging: false });//notify to plug
+consolelog('inCycle expected false. is: ' + isWithinCycle(getState()));
+setState({ cycleMonth: new Date().getMonth()-1 });
+device.battery.emit('updated', { testid:12, percentage: dischargedLimit, isCharging: true });
+consolelog('inCycle expected false. is: ' + isWithinCycle(getState()));
+
+// NOT in cycle: notify to unplug if c
+setState({ cycleMonth: new Date().getMonth()-1 });
+device.battery.emit('startedCharging', { testid:15, percentage: 95, isCharging: true });//discharge fully
+device.battery.emit('startedCharging', { testid:16, percentage: 75, isCharging: true });//discharge fully
+device.battery.emit('startedCharging', { testid:17, percentage: 35, isCharging: true });//discharge fully
+// NOT in cycle: no notify if d/c
+setState({ cycleMonth: new Date().getMonth()-1 });
+device.battery.emit('startedCharging', { testid:18, percentage: 95, isCharging: false });
+device.battery.emit('startedCharging', { testid:19, percentage: 75, isCharging: false });
+device.battery.emit('startedCharging', { testid:20, percentage: 35, isCharging: false });
+
+// *Charging events: in cycle: notify if not in 40+ c or 80- d/c range
+setState({ cycleMonth: new Date().getMonth() });
+device.battery.emit('startedCharging', { testid:21, percentage: 95, isCharging: true });//notify to unplug
+device.battery.emit('startedCharging', { testid:22, percentage: 80, isCharging: true });//notify to unplug
+device.battery.emit('startedCharging', { testid:23, percentage: 75, isCharging: true });
+device.battery.emit('startedCharging', { testid:24, percentage: 40, isCharging: true });
+device.battery.emit('startedCharging', { testid:25, percentage: 35, isCharging: true });
+device.battery.emit('stoppedCharging', { testid:26, percentage: 95, isCharging: false });
+device.battery.emit('stoppedCharging', { testid:27, percentage: 80, isCharging: false });
+device.battery.emit('stoppedCharging', { testid:28, percentage: 75, isCharging: false });
+device.battery.emit('stoppedCharging', { testid:29, percentage: 40, isCharging: false });//notify to plug
+device.battery.emit('stoppedCharging', { testid:30, percentage: 35, isCharging: false });//notify to plug
+
+// in cycle: notify to unplug if prev 80 (shallow = upper)
+setState({ cycleMonth: new Date().getMonth(), shallow: 'upper' });
+device.battery.emit('updated', { testid:31, percentage: 75, isCharging: true });//notify to unplug
+device.battery.emit('updated', { testid:32, percentage: 95, isCharging: true });//notify to unplug
+device.battery.emit('updated', { testid:33, percentage: 40, isCharging: true });//notify to unplug
+consolelog('shallow expected lower. is: ' + getState().shallow);
+
+// in cycle: notify to plug if prev 40 (shallow = lower)
+setState({ cycleMonth: new Date().getMonth(), shallow: 'lower' });
+device.battery.emit('updated', { testid:34, percentage: 45, isCharging: false });//notify to plug in
+device.battery.emit('updated', { testid:35, percentage: 35, isCharging: false });//notify to plug in
+device.battery.emit('updated', { testid:36, percentage: 80, isCharging: false });//notify to plug in
+consolelog('shallow expected upper. is: ' + getState().shallow);
+
+// revert to old state
+setState(oldstate);
+consolelog('reverting. oldstate: ' + stateLog());
+
+consolelog('tests end');
+
+*/
+//}/////////////// tests /////////////////
