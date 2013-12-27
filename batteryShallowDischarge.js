148c3429925f4538ebcfa36c72d679ec9415f4a9
hippy <hippy2981@gmail.com>
2013-12-27 10:12:37 -0800

    Changing cycle month to 1-based instead of 0-based.

--- batteryShallowDischarge.js
+++ batteryShallowDischarge.js
@@ -171,7 +171,7 @@ function notify(text, status) {
 
 // Returns true if in cycle.
 function isWithinCycle(state) {
-    var month = new Date().getMonth();
+    var month = new Date().getMonth()+1;
     var result = month === state.cycleMonth;
     consolelog((result ? 'in' : 'out') + ' cycle' + ', month: ' + month);
     return result;
@@ -182,7 +182,7 @@ function resetCycle(force, date) {
     force = force === undefined;
     date = date || new Date();
     if (force || !getState()) {
-        var state = { cycleMonth: date.getMonth(), shallow: getShallow() };
+        var state = { cycleMonth: date.getMonth()+1, shallow: getShallow() };
         setState(state);
         consolelog('resetCycle. state: ' + stateLog());
     } else {
@@ -272,46 +272,46 @@ var oldstate = getState();
 notifyInterval = -1; // disable notifyInterval
 
 // in cycle: notify only at 40 d/c and 80 c
-setState({ cycleMonth: new Date().getMonth() });
+setState({ cycleMonth: new Date().getMonth()+1 });
 device.battery.emit('updated', { testid: 1, percentage: lowerLimit, isCharging: true });
 device.battery.emit('updated', { testid: 2, percentage: lowerLimit, isCharging: false });//notify to plug
 device.battery.emit('updated', { testid: 3, percentage: upperLimit, isCharging: true });//notify to unplug
 device.battery.emit('updated', { testid: 4, percentage: upperLimit, isCharging: false });
 
 // NOT in cycle: no notify at 40 d/c and 80 c
-setState({ cycleMonth: new Date().getMonth()-1 });
+setState({ cycleMonth: new Date().getMonth()+1-1 });
 device.battery.emit('updated', { testid: 5, percentage: lowerLimit, isCharging: false });
 device.battery.emit('updated', { testid: 6, percentage: upperLimit, isCharging: true });
 
 // c limit: notify always in cycle or not but not when not c
-setState({ cycleMonth: new Date().getMonth() });
+setState({ cycleMonth: new Date().getMonth()+1 });
 device.battery.emit('updated', { testid: 7, percentage: 100, isCharging: true });//notify to unplug
 device.battery.emit('updated', { testid: 8, percentage: 100, isCharging: false });
-setState({ cycleMonth: new Date().getMonth()-1 });
+setState({ cycleMonth: new Date().getMonth()+1-1 });
 device.battery.emit('updated', { testid: 9, percentage: 100, isCharging: true });//notify to unplug
 device.battery.emit('updated', { testid:10, percentage: 100, isCharging: false });
 
 // NOT in cycle: notify at d/c limit and reset cycle only when d/c
-setState({ cycleMonth: new Date().getMonth()-1 });
+setState({ cycleMonth: new Date().getMonth()+1-1 });
 device.battery.emit('updated', { testid:11, percentage: dischargedLimit, isCharging: false });//notify to plug
 consolelog('inCycle expected false. is: ' + isWithinCycle(getState()));
-setState({ cycleMonth: new Date().getMonth()-1 });
+setState({ cycleMonth: new Date().getMonth()+1-1 });
 device.battery.emit('updated', { testid:12, percentage: dischargedLimit, isCharging: true });
 consolelog('inCycle expected false. is: ' + isWithinCycle(getState()));
 
 // NOT in cycle: notify to unplug if c
-setState({ cycleMonth: new Date().getMonth()-1 });
+setState({ cycleMonth: new Date().getMonth()+1-1 });
 device.battery.emit('startedCharging', { testid:15, percentage: 95, isCharging: true });//discharge fully
 device.battery.emit('startedCharging', { testid:16, percentage: 75, isCharging: true });//discharge fully
 device.battery.emit('startedCharging', { testid:17, percentage: 35, isCharging: true });//discharge fully
 // NOT in cycle: no notify if d/c
-setState({ cycleMonth: new Date().getMonth()-1 });
+setState({ cycleMonth: new Date().getMonth()+1-1 });
 device.battery.emit('startedCharging', { testid:18, percentage: 95, isCharging: false });
 device.battery.emit('startedCharging', { testid:19, percentage: 75, isCharging: false });
 device.battery.emit('startedCharging', { testid:20, percentage: 35, isCharging: false });
 
 // *Charging events: in cycle: notify if not in 40+ c or 80- d/c range
-setState({ cycleMonth: new Date().getMonth() });
+setState({ cycleMonth: new Date().getMonth()+1 });
 device.battery.emit('startedCharging', { testid:21, percentage: 95, isCharging: true });//notify to unplug
 device.battery.emit('startedCharging', { testid:22, percentage: 80, isCharging: true });//notify to unplug
 device.battery.emit('startedCharging', { testid:23, percentage: 75, isCharging: true });
@@ -324,14 +324,14 @@ device.battery.emit('stoppedCharging', { testid:29, percentage: 40, isCharging: 
 device.battery.emit('stoppedCharging', { testid:30, percentage: 35, isCharging: false });//notify to plug
 
 // in cycle: notify to unplug if prev 80 (shallow = upper)
-setState({ cycleMonth: new Date().getMonth(), shallow: 'upper' });
+setState({ cycleMonth: new Date().getMonth()+1, shallow: 'upper' });
 device.battery.emit('updated', { testid:31, percentage: 75, isCharging: true });//notify to unplug
 device.battery.emit('updated', { testid:32, percentage: 95, isCharging: true });//notify to unplug
 device.battery.emit('updated', { testid:33, percentage: 40, isCharging: true });
 consolelog('shallow expected lower. is: ' + getState().shallow);
 
 // in cycle: notify to plug if prev 40 (shallow = lower)
-setState({ cycleMonth: new Date().getMonth(), shallow: 'lower' });
+setState({ cycleMonth: new Date().getMonth()+1, shallow: 'lower' });
 device.battery.emit('updated', { testid:34, percentage: 45, isCharging: false });//notify to plug in
 device.battery.emit('updated', { testid:35, percentage: 35, isCharging: false });//notify to plug in
 device.battery.emit('updated', { testid:36, percentage: 80, isCharging: false });
@@ -346,16 +346,16 @@ getShallow({ testid:41, percentage: 90, isCharging: true });//upper
 getShallow({ testid:42, percentage: 90, isCharging: false });//upper
 
 // % jump tests
-setState({ cycleMonth: new Date().getMonth(), shallow: 'lower' }); lastPercentage = 75;
+setState({ cycleMonth: new Date().getMonth()+1, shallow: 'lower' }); lastPercentage = 75;
 device.battery.emit('updated', { testid:43, percentage: 79, isCharging: true });
 consolelog('shallow expected lower. is: ' + getState().shallow);
-setState({ cycleMonth: new Date().getMonth(), shallow: 'lower' }); lastPercentage = 75;
+setState({ cycleMonth: new Date().getMonth()+1, shallow: 'lower' }); lastPercentage = 75;
 device.battery.emit('updated', { testid:43, percentage: 80, isCharging: true });
 consolelog('shallow expected upper. is: ' + getState().shallow);
-setState({ cycleMonth: new Date().getMonth(), shallow: 'lower' }); lastPercentage = 75;
+setState({ cycleMonth: new Date().getMonth()+1, shallow: 'lower' }); lastPercentage = 75;
 device.battery.emit('updated', { testid:44, percentage: 81, isCharging: true });
 consolelog('shallow expected upper. is: ' + getState().shallow);
-setState({ cycleMonth: new Date().getMonth(), shallow: 'lower' }); lastPercentage = 75;
+setState({ cycleMonth: new Date().getMonth()+1, shallow: 'lower' }); lastPercentage = 75;
 device.battery.emit('updated', { testid:45, percentage: 86, isCharging: true });
 consolelog('shallow expected upper. is: ' + getState().shallow);
 
